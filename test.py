8f58a5faef9cd032aefdcd385b3990274ab9a51a
YaverMS10 <yaverms10@gmail.com>
2020-12-02 16:26:35 +0400

    Input elave edildi

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+a = input("Istifadeci adini daxil edin: ")
 def quvvetin_hesablanmasi(x, p):
     ''' ededin quvvetini hesablamaq ucun proqram
  x = eded
  p = quvvet
     '''
-    print(f"{x}^{p}={pow(x, p)}")
+    print(f"{a}, {x}^{p}={pow(x, p)}")
 print(quvvetin_hesablanmasi.__doc__)
+
