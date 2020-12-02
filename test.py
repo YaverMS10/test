1543c62691e3e113666c9fd9d988f78fdc63c74e
YaverMS10 <yaverms10@gmail.com>
2020-12-02 16:19:37 +0400

    Fayl elave edildi

---
+++ test.py
@@ -0,0 +1,7 @@
+def quvvetin_hesablanmasi(x, p):
+    ''' ededin quvvetini hesablamaq ucun proqram
+ x = eded
+ p = quvvet
+    '''
+    print(f"{x}^{p}={pow(x, p)}")
+print(quvvetin_hesablanmasi.__doc__)
